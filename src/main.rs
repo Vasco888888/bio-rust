f17b327e61a643952682ddc60bd515466b7d8401
Vasco <up202306731@up.pt>
2026-02-12 21:30:45 +0000

    Add WGSL shader and Vertex layout description

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,34 @@ use winit::{
     window::WindowBuilder,
 };
 
+use wgpu::*;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
     position: [f32; 2],
     color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x2,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 2]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
 }
 
 const VERTICES: &[Vertex] = &[
