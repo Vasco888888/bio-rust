e45e5475f60b2b6903ec899d1e6b2469c3c7d27e
Vasco <up202306731@up.pt>
2026-02-12 21:09:01 +0000

    Implement basic winit window and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+
+    let _window = WindowBuilder::new()
+        .with_title("Bio Rust: First Step")
+        .build(&event_loop)
+        .unwrap();
+
+    println!("Running");
+    event_loop.run(move |event, target| {
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, ..} => {
+                println!("Closing");
+                target.exit();
+            }
+            _ => {},
+        }
+    }).unwrap();
 }
